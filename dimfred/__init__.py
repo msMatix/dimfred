9a2418c78451eeaa5dd840f5da9c588298ba7930
Dmitrij Vinokour <dimfred.1337@web.de>
2022-03-28 14:10:13 +0200

    pipe

--- dimfred/__init__.py
+++ dimfred/__init__.py
@@ -1,6 +1,7 @@
 from .worker import Worker, Queue
 from .stopwatch import Stopwatch
 from .config import Config
+from .pipe import fpipe, pipe
 
 from tabulate import tabulate
 from easydict import EasyDict as edict
