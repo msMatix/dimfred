f6acd215b2d8eb6921405105ff9b7287b6c22be5
Dmitrij Vinokour <dimfred.1337@web.de>
2022-07-09 11:38:25 +0200

    feat: remove config from base package import

--- dimfred/__init__.py
+++ dimfred/__init__.py
@@ -8,7 +8,6 @@ from pluck import pluck
 from tabulate import tabulate
 
 from . import fastapi_utils
-from .config import BaseConfig, ABI
 from .loop import loop
 from .pipe import fpipe, pipe
 from .psutil_ext import *
