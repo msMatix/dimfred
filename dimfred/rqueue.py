944076c144e5c83a318f9da1a1223772bd1d2189
Dmitrij Vinokour <dimfred.1337@web.de>
2022-07-15 20:38:22 +0200

    fix: catch ex, return pop

--- dimfred/rqueue.py
+++ dimfred/rqueue.py
@@ -5,15 +5,19 @@ class RQueue:
     def __init__(self, name, url="127.0.0.1:6379", options=None, **kwargs):
         host, *port = url.split(":")
         port = 6379 if not port else port[0]
-
+        port = 6379 if not port else port[0]
         self.queue = RedisSMQ(host=host, port=port, options=options, **kwargs)
-        self.queue.createQueue(qname=name).execute()
+
+        try:
+            self.queue.createQueue(qname=name).execute()
+        except Exception:
+            pass
 
     def push(self, msg):
         self.queue.sendMessage(message=msg).execute()
 
     def pop(self, **kwargs):
         try:
-            self.queue.popMessage(**kwargs)
-        except:
+            return self.queue.popMessage(**kwargs)
+        except Exception:
             return None
